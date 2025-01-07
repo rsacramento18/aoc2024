6adcdb25a12d4a35850c3c60aca871991e4efbdb
Ricardo Sacramento <ricardo.sacramento18@gmail.com>
2025-01-07 22:33:10 +0000

    feat: adding attempt on day2

--- cmd/pakage/day2/day2.go
+++ cmd/pakage/day2/day2.go
@@ -58,18 +58,13 @@ func part2(arr [][]string) {
 		b, _ := strconv.Atoi(report[1])
 		increasing := b-a > 0
 
-		fmt.Println(report)
 		isSafe := true
-		err := false
+		error := false
+		fmt.Println(report)
 		for i := range report {
 			if i > 0 {
 				curr, _ := strconv.Atoi(report[i])
-				var before int
-				if err == true {
-					before, _ = strconv.Atoi(report[i-2])
-				} else {
-					before, _ = strconv.Atoi(report[i-1])
-				}
+				before, _ := strconv.Atoi(report[i-1])
 
 				diffIncreasing := curr - before
 				diffDecreasing := before - curr
@@ -79,18 +74,29 @@ func part2(arr [][]string) {
 				} else {
 					isSafe = 1 <= diffDecreasing && diffDecreasing <= 3
 				}
-				if isSafe == false && err == false {
+				if isSafe == false && error == false {
 					isSafe = true
-					err = true
-				} else if isSafe == false && err == true {
+					error = true
+					fmt.Println("--------", i)
+					report = remove(report, i)
+					fmt.Println(report)
+					i = 0
+				} else if error == true {
 					break
 				}
 			}
 		}
-		fmt.Println("isSafe", isSafe)
 		if isSafe == true {
 			count += 1
 		}
 	}
-	fmt.Println("Day2 ---- part2 ---- count ----", count)
+	fmt.Println("Day2 ---- part1 ---- count ----", count)
+}
+
+func remove(slice []string, s int) []string {
+	if s < 0 || s >= len(slice) {
+		return append(slice[:s], slice[s+1:]...)
+	} else {
+		return slice
+	}
 }
