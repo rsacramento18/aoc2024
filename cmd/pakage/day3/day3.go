cbeefa386d4ad985d0433ab4070be6689619e1f5
Ricardo Sacramento <sacramento@webeo.com>
2025-01-09 14:03:02 +0000

    feat:adding day3 part1 and part2

--- cmd/pakage/day3/day3.go
+++ cmd/pakage/day3/day3.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/rsacramento18/aoc2024/cmd/pakage/file"
 )
 
 func Run() {
-	lines := file.ReadFile("../pakage/day3/input.txt")
+	lines := file.ReadFile("../pakage/day3/input4.txt")
 	part1(lines)
+	part2(lines)
 }
 
 func part1(arr []string) {
@@ -18,18 +20,55 @@ func part1(arr []string) {
 	r := regexp.MustCompile(`mul\(\d*,\d*\)`)
 	for _, line := range arr {
 		matches := r.FindAllStringSubmatch(line, -1)
-		fmt.Println("matches", matches)
 
 		for _, operation := range matches {
-			fmt.Println("operation", operation[0])
-			fmt.Println("num1", string(operation[0][4]))
-			fmt.Println("num2", string(operation[0][6]))
+			substr := string(operation[0][4 : len(operation[0])-1])
+			s := strings.Split(substr, ",")
 
-			num1, _ := strconv.Atoi(string(operation[0][4]))
-			num2, _ := strconv.Atoi(string(operation[0][6]))
+			num1, _ := strconv.Atoi(s[0])
+			num2, _ := strconv.Atoi(s[1])
 
 			sum += num1 * num2
 		}
 	}
 	fmt.Println("Day3 ---- part1 ---- sum ----", sum)
+}
+
+func part2(arr []string) {
+	sum := 0
+	r := regexp.MustCompile(`(don't\(\)|do\(\))|(mul\(\d*,\d*\))`)
+	enabled := true
+	for _, line := range arr {
+		matches := r.FindAllString(line, -1)
+
+		for _, match := range matches {
+			switch match {
+			case "do()":
+				enabled = true
+			case "don't()":
+				enabled = false
+			default:
+				if enabled == true {
+					substr := string(match[4 : len(match)-1])
+					s := strings.Split(substr, ",")
+
+					num1, _ := strconv.Atoi(s[0])
+					num2, _ := strconv.Atoi(s[1])
+
+					sum += num1 * num2
+				}
+			}
+		}
+	}
+	fmt.Println("Day3 ---- part2 ---- sum ----", sum)
+}
+
+func checkWord(s string) string {
+	for i, char := range s {
+		if string(char) == "(" {
+			word := string(s[0:i])
+			return word
+		}
+	}
+	return ""
 }
