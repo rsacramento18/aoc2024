fe7894be6f0bdafbe74365cc0ed242b5d5f82e2d
Ricardo Sacramento <ricardo.sacramento18@gmail.com>
2025-01-10 08:57:25 +0000

    feat:day4 part1 starting

--- cmd/pakage/day4/day4.go
+++ cmd/pakage/day4/day4.go
@@ -1,8 +1,36 @@
 package day4
 
+import (
+	"fmt"
+
+	"github.com/rsacramento18/aoc2024/cmd/pakage/file"
+)
+
 func Run() {
-	part1()
+	lines := file.ReadFile("../pakage/day4/input.txt")
+	arr := file.FromLinesToArray(lines)
+	part1(arr)
 }
 
-func part1() {
+type Coordinate int
+
+const (
+	N Coordinate = iota
+	NE
+	E
+	SE
+	S
+	SW
+	W
+	NW
+)
+
+func part1(arr [][]string) {
+}
+
+func checkN(arr [][]string, x int, y int) (string, error) {
+	if y > 0 {
+		return arr[x-1][y], nil
+	}
+	return "", fmt.Errorf("Coordinate out of bounds")
 }
